0026a715720cf7f7dc9f6f5be1f3a8612c80d6b3
Akpotor <94068806+aboutWE@users.noreply.github.com>
2022-04-25 21:52:57 +0100

    post
    Car Website Project

--- hsam.js
+++
@@ -1,6 +0,0 @@
-
-var toTop = document.getElementsByClassName("to_top");
-window.addEventListener("scroll", () => {
-if (window.pageYOffset => 300px) {toTop.style.display("block");} 
-else {toTop.style.display("none");}
-})
